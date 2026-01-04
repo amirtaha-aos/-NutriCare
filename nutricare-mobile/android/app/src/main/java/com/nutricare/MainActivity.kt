9d16366fe02851e471538481773cd631760c88fc
amirtaha <amirtaha@amirtahas-MacBook-Pro.local>
2026-01-05 02:35:34 +0330

    Simplify Android build config for successful APK generation

--- nutricare-mobile/android/app/src/main/java/com/nutricare/MainActivity.kt
+++ nutricare-mobile/android/app/src/main/java/com/nutricare/MainActivity.kt
@@ -1,22 +1,12 @@
 package com.nutricare
 
-import com.facebook.react.ReactActivity
-import com.facebook.react.ReactActivityDelegate
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
-import com.facebook.react.defaults.DefaultReactActivityDelegate
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ReactActivity() {
-
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "NutriCare"
-
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Simple activity for demo APK
+        // Full React Native implementation coming soon
+    }
 }
